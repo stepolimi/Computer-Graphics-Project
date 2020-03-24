9775b9526b09832fd93d5b8f2d1c4530cc0b9a04
stepolimi <37816183+stepolimi@users.noreply.github.com>
2020-03-24 02:21:20 +0100

    Assignment n.6

--- Assignments/Assignment_06/A06 - World and LookAt view matrices/WorldView.js
+++ Assignments/Assignment_06/A06 - World and LookAt view matrices/WorldView.js
@@ -7,8 +7,32 @@ function worldViewProjection(carx, cary, carz, cardir, camx, camy, camz) {
 // The camera is placed at position camx, camy and camz. The view matrix should be computed using the
 // LookAt camera matrix procedure, with the correct up-vector.
 
-	var world = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
-	var view  = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
+	
+	// View matrix
+
+	var car = [carx, cary, carz];
+	var cam = [camx, camy, camz];
+	var uy = [0,1,0];
+
+	var cc = [cam[0]-car[0], cam[1]-car[1], cam[2]- car[2]];
+	var vz = utils.normalizeVector3(cc);
+	var vxx = utils.crossVector(uy, vz);
+	var vx = utils.normalizeVector3(vxx);
+	var vy = utils.crossVector(vz, vx);
+	
+	var view  =    [vx[0],  vx[1],  vx[2],  -vx[0]*cam[0]-vx[1]*cam[1]-vx[2]*cam[2], 
+			vy[0],  vy[1],  vy[2],  -vy[0]*cam[0]-vy[1]*cam[1]-vy[2]*cam[2], 
+			vz[0],  vz[1],  vz[2],  -vz[0]*cam[0]-vz[1]*cam[1]-vz[2]*cam[2], 
+			0,  	0,  	0,  	1];
+
+
+	//World matrix
+
+	var rot = utils.MakeRotateYMatrix(cardir);
+	var transl = utils.MakeTranslateMatrix(carx,cary,carz);
+	
+	var world = utils.multiplyMatrices(transl,rot);
+
 
 	return [world, view];
 }
