469d4ae84f7a4d68572ff80c8917695c572d434f
alepas <alessandra.pasini@mail.polimi.it>
2020-11-14 15:58:33 +0100

    fix

--- Hungry-Birds-Simulator/scriptCanvas.js
+++ Hungry-Birds-Simulator/scriptCanvas.js
@@ -74,9 +74,9 @@ var normalTransformationMatrix;
 //camera variables
 var cx = 0;
 var cy = 0;
-var cz = 10;
+var cz = -10;
 var elev = 0;
-var ang = 0;
+var ang = 180;
 var vx = 0;
 var vy = 0;
 var vz = 0;
