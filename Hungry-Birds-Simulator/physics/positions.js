bd1cc18758cabbc0a13adadfac4aeb7ea93b27b3
alepas <alessandra.pasini@mail.polimi.it>
2020-11-14 17:48:14 +0100

    Update positions.js

--- Hungry-Birds-Simulator/physics/positions.js
+++ Hungry-Birds-Simulator/physics/positions.js
@@ -1,4 +1,4 @@
-var slingMatrix = utils.MakeWorld(-4.0, 0.0 , 0.0, 0.0, 0.0, 90.0, 1);
+var slingMatrix = utils.MakeWorld(-4.0, 0.0 , 0.0, 0.0, 90.0, 0.0, 1);
 var redMatrix = utils.MakeWorld(0.0, 0.0 , 0.0, 0.0, 0.0, 0.0, 1);
 
 var worldPositions = [
