cc3611b5c106c6e1b7fb3dc2ec9634127b28f987
stepolimi <37816183+stepolimi@users.noreply.github.com>
2021-01-03 11:53:26 +0100

    Update positions.js

--- Hungry-Birds-Simulator/physics/positions.js
+++ Hungry-Birds-Simulator/physics/positions.js
@@ -1,5 +1,5 @@
 var envMatrix = utils.MakeWorld(0.0, 0.0 , 0.0, 0.0, 0.0, 0.0, 1);
-var slingMatrix = utils.MakeWorld(-4.0, 0.0 , 0.0, 90.0, 0.0, 0.0, 0.5);
+var slingMatrix = utils.MakeWorld(-40.0, 0.0 , 0.0, 90.0, 0.0, 0.0, 0.5);
 var redMatrix = utils.MakeWorld(0.0, 0.0 , 0.0, 0.0, 0.0, 0.0, 1);
 
 var worldPositions = [
