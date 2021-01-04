21a14ff7d3e8cd85e2cab3cb36f3cd59c331779c
stepolimi <37816183+stepolimi@users.noreply.github.com>
2021-01-04 19:17:30 +0100

    Update positions.js

--- Hungry-Birds-Simulator/physics/positions.js
+++ Hungry-Birds-Simulator/physics/positions.js
@@ -7,17 +7,17 @@ var bird2 = utils.MakeWorld(-0.5, 0.0 , -7.5, 0.0, 0.0, 0.0, 0.5);
 var bird3 = utils.MakeWorld(-1.5, 0.0 , -7.5, 0.0, 0.0, 0.0, 0.5);	
 var bird4 = utils.MakeWorld(-2.5, 0.0 , -7.5, 0.0, 0.0, 0.0, 0.5);	
 var bird5 = utils.MakeWorld(-3.5, 0.0 , -7.5, 0.0, 0.0, 0.0, 0.5);	
-var pig1 = utils.MakeWorld(0.0, 0.0 , 2.0, 0.0, 0.0, 0.0, 1);
-var	pig2 = utils.MakeWorld(0.0, 0.0 , 4.0, 0.0, 0.0, 0.0, 1);
-var pig3 = utils.MakeWorld(0.0, 1.4 , 3.0, 0.0, 0.0, 0.0, 1);
+var pig1 = utils.MakeWorld(0.0, 0.0 , 2.0, 90.0, 0.0, 0.0, 1);
+var	pig2 = utils.MakeWorld(0.0, 0.0 , 4.0, 0.0, 90.0, 0.0, 1);
+var pig3 = utils.MakeWorld(0.0, 1.6 , 3.0, 0.0, 0.0, 90.0, 1);
 var tower11 = utils.MakeWorld(0.0, 0.0 , 1.0, 0.0, 0.0, 0.0, 0.2);
-var tower12 = utils.MakeWorld(0.0, 0.7 , 1.0, 0.0, 0.0, 0.0, 0.2);
-var tower13 = utils.MakeWorld(0.0, 1.4 , 1.0, 0.0, 0.0, 0.0, 0.2);
-var tower14 = utils.MakeWorld(0.0, 2.1 , 1.0, 0.0, 0.0, 0.0, 0.2);
+var tower12 = utils.MakeWorld(0.0, 0.8 , 1.0, 0.0, 0.0, 0.0, 0.2);
+var tower13 = utils.MakeWorld(0.0, 1.6 , 1.0, 0.0, 0.0, 0.0, 0.2);
+var tower14 = utils.MakeWorld(0.0, 2.4 , 1.0, 0.0, 0.0, 0.0, 0.2);
 var tower21 = utils.MakeWorld(0.0, 0.0 , 3.0, 0.0, 0.0, 0.0, 0.2);
-var tower22 = utils.MakeWorld(0.0, 0.7 , 3.0, 0.0, 0.0, 0.0, 0.2);
+var tower22 = utils.MakeWorld(0.0, 0.8 , 3.0, 0.0, 0.0, 0.0, 0.2);
 var tower31 = utils.MakeWorld(0.0, 0.0 , 5.0, 0.0, 0.0, 0.0, 0.2);
-var tower32 = utils.MakeWorld(0.0, 0.7 , 5.0, 0.0, 0.0, 0.0, 0.2);
+var tower32 = utils.MakeWorld(0.0, 0.8 , 5.0, 0.0, 0.0, 0.0, 0.2);
 
 
 var worldPositions = [
