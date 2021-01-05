5d5b6c44da0b8cd769b878b6707219ae1b85beaa
alepas <alessandra.pasini@mail.polimi.it>
2021-01-05 22:52:39 +0100

    change of elastic proportion

--- Hungry-Birds-Simulator/physics/positions.js
+++ Hungry-Birds-Simulator/physics/positions.js
@@ -18,7 +18,7 @@ var tower21 = utils.MakeWorld(0.0, 0.0 , 3.0, 0.0, 0.0, 0.0, 0.2);
 var tower22 = utils.MakeWorld(0.0, 0.8 , 3.0, 0.0, 0.0, 0.0, 0.2);
 var tower31 = utils.MakeWorld(0.0, 0.0 , 5.0, 0.0, 0.0, 0.0, 0.2);
 var tower32 = utils.MakeWorld(0.0, 0.8 , 5.0, 0.0, 0.0, 0.0, 0.2);
-var slingElasticMatrix = utils.MakeWorld(0, 1.0 , -7.0, 0.0, 0.0, 0.0, 7);
+var slingElasticMatrix = utils.MakeWorld(0, 1.0 , -7.0, 0.0, 0.0, 0.0, 1.0);
 
 
 var worldPositions = [
