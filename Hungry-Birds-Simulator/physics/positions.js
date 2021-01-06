45b9762b76e4bd1965dfe70e455d2a7065c376c5
alepas <alessandra.pasini@mail.polimi.it>
2021-01-07 00:12:36 +0100

    Update positions.js

--- Hungry-Birds-Simulator/physics/positions.js
+++ Hungry-Birds-Simulator/physics/positions.js
@@ -107,6 +107,6 @@ function scaleSlingElasticZ(){
 	if (counter < 5){
 		var variation = 7.2 + elasticScalingZ*5.5;
 		// +variation*Math.sin(utils.degToRad(angleY))
-		worldPositions[2 + counter] = utils.MakeWorld(0.0 +variation*Math.sin(utils.degToRad(angleY)) , 1.1 + variation*Math.sin(utils.degToRad(angleY)), -variation, 0.0,  angleY, 0.0, 0.5);
+		worldPositions[2 + counter] = utils.MakeWorld(0.0 , 1.1 + variation*Math.sin(utils.degToRad(angleY)), -variation*Math.sin(utils.degToRad(angleY)) , 0.0,  angleY, 0.0, 0.5);
 	}
 }
