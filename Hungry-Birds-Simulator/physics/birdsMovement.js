c5c16c7a56a69de3ac9556ee5472779cc9811d43
alepas <alessandra.pasini@mail.polimi.it>
2021-01-07 12:49:27 +0100

    changes

--- Hungry-Birds-Simulator/physics/birdsMovement.js
+++ Hungry-Birds-Simulator/physics/birdsMovement.js
@@ -1,25 +1,31 @@
 // JavaScript source code
-var t = 0.0001;
+var t = 0;
+var lastUpdateTime = (new Date).getTime(); 
 
 function birdTrajectory(index){
 	var angle = Math.abs(angleY);
 	var v = BIRD_SPEED * variation;
 	var g = -0.8;	
 
+
 	//get the time
     var currentTime = (new Date).getTime();
+	if(lastUpdateTime)
+      var t += (30 * (currentTime - lastUpdateTime)) / 1000.0;
 
 
 	var y = v*t*Math.sin(utils.degToRad(angle)) - (g*t*t /2);
 	var z = v*t*Math.cos(utils.degToRad(angle));
 
-	console.log("angle" + angleY);
+	console.log("angle" + angle);
 	console.log("y " + y);
 	console.log("z " + z);
 	console.log("t "+ t);
 	console.log("------------------------");
 
 	worldPositions[index] = utils.MakeWorld(0.0 , y, z , 0.0,  angleY, 0.0, 0.5);
-	t += 0.0001;
+	
+	
+    lastUpdateTime = currentTime;     
 	//birdTrajectory(index);
 }
