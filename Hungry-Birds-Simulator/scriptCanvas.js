cde20477a2ebd14ec71bf31254a3374c1e0d4dce
stepolimi <37816183+stepolimi@users.noreply.github.com>
2021-01-20 19:20:28 +0100

    Update scriptCanvas.js

--- Hungry-Birds-Simulator/scriptCanvas.js
+++ Hungry-Birds-Simulator/scriptCanvas.js
@@ -601,3 +601,39 @@ function addMeshToScene(i) {
 window.onload = main;
 
 
+//applies gravity to objects
+function checkStability(){
+    structureObjs.forEach(function(objTocheck) {
+        let objY = objTocheck.ty - objTocheck.rady;
+        let objZ = objTocheck.tz;
+        let objZStart = objTocheck.tz - objTocheck.radz;
+        let objZEnd = objTocheck.tz + objTocheck.radz;
+        let stable = false;
+        let precStable = false;
+        let nextStable = false;
+        let tollerance = 0.05;
+        structureObjs.forEach(function(obj) {
+            if((obj.ty + obj.rady > objY - tollerance) && (obj.ty + obj.rady < objY + tollerance)){
+                if((obj.tz + obj.radz >= objZ) || (obj.tz - obj.radz <= objZ))
+                    stable = true;
+                else if(obj.tz + obj.radz >= objZStart)
+                    precStable = true;
+                else if(obj.tz - obj.radz <= objZEnd)
+                    nextStable = true;
+            }
+        });
+        if(stable || (precStable && nextStable))
+            objTocheck.isStable = false;
+    });
+}
+
+function objectFall(){
+    structureObjs.forEach(function(obj) {
+       if(!obj.isStable){
+           //todo: scaling
+        obj.vy = obj.vy - (g*TICK*TICK /2);
+        obj.ty = obj.ty + obj.vy * TICK;
+        worldPositions[obj.index] = utils.MakeWorld(obj.tx , obj.ty, obj.tz, obj.rx, obj.ry, obj.rz, 0.2);
+       }
+    });
+}
