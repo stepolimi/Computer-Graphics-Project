4397d0cc32422bae2292bff57385d9e9e49a8a58
stepolimi <37816183+stepolimi@users.noreply.github.com>
2021-01-21 13:12:13 +0100

    Update castlePiece.js

--- Hungry-Birds-Simulator/physics/castlePiece.js
+++ Hungry-Birds-Simulator/physics/castlePiece.js
@@ -1,6 +1,6 @@
 // JavaScript source code
 class structureObjects {
-	constructor(tx, ty, tz, rx, ry, rz, type, index){
+	constructor(tx, ty, tz, rx, ry, rz, type, index, scale){
 		this.tx = tx;
 		this.ty = ty;
 		this.tz = tz;
@@ -20,35 +20,17 @@ class structureObjects {
 		if(type == "glassVerticalPlane" ||  type == "woodVerticalPlane"){
 			this.rady = VERTICAL_PANE_BIG_RADIUS;
 			this.radz = VERTICAL_PANE_SMALL_RADIUS;
-		}
-		else if(type == "glassHorizontalPlane" ||  type == "woodHorizontalPlane"){
+		} else if(type == "glassHorizontalPlane" ||  type == "woodHorizontalPlane"){
 			this.rady = HORIZONTAL_PANE_SMALL_RADIUS;
 			this.radz =  HORIZONTAL_PANE_BIG_RADIUS;
+		} else if(type.includes("pig")){
+			this.scale = scale;
+			this.rady = scale / 2;
+			this.radz = rady;
 		} 
 		else{
 			this.rady = STRUCTURE_OBJ_RADIUS;
 			this.radz = STRUCTURE_OBJ_RADIUS;
 		}
-	}
-	
-	//used for pigs with different scaling
-	constructor(tx, ty, tz, rx, ry, rz, type, index, scale){
-		this.tx = tx;
-		this.ty = ty;
-		this.tz = tz;
-		this.rx = rx;
-		this.ry = ry;
-		this.rz = rz;
-		this.vz = 0;
-		this.vy = 0;
-		this.scale = scale;
-		this.index = index;
-		this.type = type;
-		this.isColliding = false;
-		this.isStable = true;
-		this.supLeftPieces = [];
-		this.supRightPieces = [];
-		this.rady = scale / 2;
-		this.radz = rady;
 	}
 }
