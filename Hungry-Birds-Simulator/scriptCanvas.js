67b65e482365f15f69bb5cdad95dda02c72e27e8
stepolimi <37816183+stepolimi@users.noreply.github.com>
2021-02-02 22:08:40 +0100

    Update scriptCanvas.js

--- Hungry-Birds-Simulator/scriptCanvas.js
+++ Hungry-Birds-Simulator/scriptCanvas.js
@@ -505,15 +505,20 @@ var sunAngle = 0;
 
 //sets light colors and positions
 function setupLights(){
+    var xDirLightA;
     var ambientLight = [0.4, 0.4, 0.4];
 
     if(sunAngle < 2* Math.PI)
         sunAngle += Math.PI/1000;
     else
         sunAngle = 0;
+    if(sunAngle >= 0 && sunAngle < Math.PI) 
+        xDirLightA = -0.2;
+    else
+        xDirLightA = 0;
     
         //x to be -0.2 on day, -0 on night
-    var directionaLightAPos = [-0.2, 0.5 * Math.sin(sunAngle), 0.5 * Math.cos(sunAngle)];
+    var directionaLightAPos = [xDirLightA, 0.5 * Math.sin(sunAngle), 0.5 * Math.cos(sunAngle)];
     var directionalLightAColor = [1.0, 0.8, 0.1];//#4d4d4d
 
     var lightDirectionalMatrix = utils.sub3x3from4x4(utils.invertMatrix(utils.transposeMatrix(viewMatrix)));
