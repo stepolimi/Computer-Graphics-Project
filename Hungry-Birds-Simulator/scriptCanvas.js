626aa4f68c0d4472f136af578b75e9b6707230d8
stepolimi <37816183+stepolimi@users.noreply.github.com>
2021-02-05 02:12:32 +0100

    Update scriptCanvas.js

--- Hungry-Birds-Simulator/scriptCanvas.js
+++ Hungry-Birds-Simulator/scriptCanvas.js
@@ -559,7 +559,7 @@ function setupLights(){
     var diffuseLightPosition = [dirLightAlphaA, dirLightBetaA, dirLightGammaA];
     var diffuseLightColor = [0.9, 0.9, 0.9];
     //Transform the diffuse light's Position into Camera Space
-    var diffuseLightPosTransfMatrix = utils.sub3x3from4x4(utils.invertMatrix(viewMatrix));
+    var diffuseLightPosTransfMatrix = utils.sub3x3from4x4(viewMatrix);
     var diffuseLightPosTransform = utils.normalizeVector3(utils.multiplyMatrix3Vector3(diffuseLightPosTransfMatrix,diffuseLightPosition));
     //var diffuseLightPosTransform = utils.multiplyMatrixVector(viewMatrix,diffuseLightPosition);
 
