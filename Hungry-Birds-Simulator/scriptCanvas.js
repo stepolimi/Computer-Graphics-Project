5760711d3cec65e1c46dd90d8c5e65521d0c5daf
stepolimi <37816183+stepolimi@users.noreply.github.com>
2021-02-05 01:53:32 +0100

    blinn light

--- Hungry-Birds-Simulator/scriptCanvas.js
+++ Hungry-Birds-Simulator/scriptCanvas.js
@@ -556,10 +556,10 @@ function setupLights(){
     var dirLightAlphaA = document.getElementById("dirLightAlphaA").value;//20
     var dirLightBetaA = document.getElementById("dirLightBetaA").value;//32
     var dirLightGammaA = document.getElementById("dirLightGammaA").value;//32
-    var diffuseLightPosition = [0, 0.0, -1.0];
+    var diffuseLightPosition = [dirLightAlphaA, dirLightBetaA, dirLightGammaA];
     var diffuseLightColor = [0.9, 0.9, 0.9];
     //Transform the diffuse light's Position into Camera Space
-    var diffuseLightPosTransfMatrix = utils.sub3x3from4x4(utils.invertMatrix(utils.transposeMatrix(viewMatrix)));
+    var diffuseLightPosTransfMatrix = utils.sub3x3from4x4(utils.transposeMatrix(worldViewMatrix));
     var diffuseLightPosTransform = utils.normalizeVector3(utils.multiplyMatrix3Vector3(diffuseLightPosTransfMatrix,diffuseLightPosition));
     //var diffuseLightPosTransform = utils.multiplyMatrixVector(viewMatrix,diffuseLightPosition);
 
