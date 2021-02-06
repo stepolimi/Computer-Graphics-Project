540457656ed794993fb00b622c77649c18bb6c72
alepas <alessandra.pasini@mail.polimi.it>
2021-02-07 00:59:09 +0100

    Update scriptCanvas.js

--- Hungry-Birds-Simulator/scriptCanvas.js
+++ Hungry-Birds-Simulator/scriptCanvas.js
@@ -83,8 +83,8 @@ void main() {
 
   //compute spot light
   vec4 spotAPos = lightDiffusePosition - fs_pos;
-  vec3 spotCol = lightDiffuseColor *  vec3(dot(pow(spotATarget/length(spotAPos), spotADecay), 
-  clamp((dot(normalize(spotAPos), spotADir) - cos(radians(spotAConeOut)/2.0)) / (cos(radians(spotAConeIn * spotAConeOut)/2.0) - cos(radians(spotAConeOut)/2.0)), 0.0, 1.0)));
+  vec4 spotCol = vec4(lightDiffuseColor, 1.0) *  dot(pow(spotATarget/length(spotAPos), spotADecay), 
+  clamp((dot(normalize(spotAPos), spotADir) - cos(radians(spotAConeOut)/2.0)) / (cos(radians(spotAConeIn * spotAConeOut)/2.0) - cos(radians(spotAConeOut)/2.0)), 0.0, 1.0));
 
   outColor = vec4(clamp(spotCol,0.0,1.0).rgb, 1.0) *  texture(in_texture, fsUV);
    //outColor = vec4(clamp(color,0.0,1.0).rgb, 1.0);
