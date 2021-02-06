012a43013b85c72adc8a740c8792fc100aec1067
alepas <alessandra.pasini@mail.polimi.it>
2021-02-06 23:42:58 +0100

    Update scriptCanvas.js

--- Hungry-Birds-Simulator/scriptCanvas.js
+++ Hungry-Birds-Simulator/scriptCanvas.js
@@ -605,8 +605,8 @@ function setupLights(){
     //SpotLight
     var spotTarget = 30.0;
     var spotDecay = 2.0;
-    var spotConeOut = 100.0;
-    var spotConeIn = 35.0;
+    var spotConeOut = 20.0;
+    var spotConeIn = 10.0;
 
 
     gl.uniform1f(spotATargetHandle, spotTarget);
