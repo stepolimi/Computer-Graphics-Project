a3404b4b2a69e9479536b515e93c117ebb6b42e7
alepas <alessandra.pasini@mail.polimi.it>
2021-02-07 15:05:45 +0100

    Update scriptCanvas.js

--- Hungry-Birds-Simulator/scriptCanvas.js
+++ Hungry-Birds-Simulator/scriptCanvas.js
@@ -147,8 +147,8 @@ void main() {
     
     
     vec4 blinnTot = (specularToSpotA + specularToSpotB + specularToSpotC + specularToSpotD);
-    //outColor = vec4(clamp(vec3(spotCol + spotBCol + spotCCol + spotDCol + blinnTot  ) + ambient + diffA ,0.0,1.0).rgb, 1.0) *  texture(in_texture, fsUV);
-    outColor = vec4(clamp(vec3(blinnTot),0.0,1.0).rgb, 1.0);
+    outColor = vec4(clamp(vec3(spotCol + spotBCol + spotCCol + spotDCol + blinnTot  ) + ambient + diffA ,0.0,1.0).rgb, 1.0) *  texture(in_texture, fsUV);
+    //outColor = vec4(clamp(vec3(blinnTot),0.0,1.0).rgb, 1.0);
     //outColor = vec4(fsUV, 0.0, 1.0);
 }
 `;
