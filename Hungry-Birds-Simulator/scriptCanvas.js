39bc8028cf17b1b243799096fffb90945aa1933f
alepas <alessandra.pasini@mail.polimi.it>
2021-02-07 23:22:43 +0100

    Update scriptCanvas.js

--- Hungry-Birds-Simulator/scriptCanvas.js
+++ Hungry-Birds-Simulator/scriptCanvas.js
@@ -127,7 +127,7 @@ void main() {
     vec3 diffA = lightColorA * clamp(dot(nNormal, nLightDirectionA), 0.0, 1.0);
 
     //----Phong on directionalA-------------------------------------------
-    vec4 dirAPhong =  pow(clamp(dot(eyePos, -reflect(vec4(lightDirectionA, 1.0), n4Normal)),0.0,1.0), phongShininess) * vec4(lightColorA, 1.0);
+    vec4 dirAPhong =  pow(clamp(dot(eyePos, -reflect(vec4(lightDirectionA, 1.0), n4Normal)),0.0,1.0), phongShininess) * vec4(lightColorA, 1.0) * vec4(specColor, 1.0);
 
 
     //----SPOTLIGHT A + Blinn--------------------------------------------
